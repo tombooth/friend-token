71d224b7eecda9b7ffd2956f06a759c95de5579b
Tom Booth <tombooth@gmail.com>
2013-11-16 00:17:16 +0000

    Add tools.logging as dep and bump minor version

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.tombooth/friend-token "0.1.0-SNAPSHOT"
+(defproject com.tombooth/friend-token "0.1.1-SNAPSHOT"
   :description "A token workflow for the Friend authentication middleware"
   :url "https://github.com/tombooth/friend-token"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [ring/ring-core "1.2.0"]
                  [com.cemerick/friend "0.1.5"]
                  [clj-time "0.5.1"]
